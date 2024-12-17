7de4a18108321611000520d5d999e5019e6a2f0f
geekr-ml <demowindowswebhost@gmail.com>
2024-12-17 14:21:09 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -1,73 +1,45 @@
-from flask import Flask
-import pickle
-import numpy as np
-
 from flask import Blueprint, render_template, request
+from .model import load_model, make_prediction
 
-model_path = "final_model_for_TBTO.pkl"
+main = Blueprint("main", __name__)
 
-with open(model_path, "rb") as f:
-        model = pickle.load(f)
+# Load the ML model
+model = load_model()
 
-app = Flask(__name__)
-
-
-def make_prediction(model, features):
-    features = np.array([features])
-    prediction = model.predict(features)[0]
-    return prediction
-
-@app.route("/")
+@main.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/predict", methods=["POST"])
+@main.route("/predict", methods=["POST"])
 def predict():
     try:
         form_data = request.form
-
         print("Received Form Data:", form_data)  # Log form data for debugging
 
         # Parse inputs into a list for prediction
         features = [
-            int(form_data.get("types_of_cases")),
-            int(form_data.get("site_of_tb_disease")),
-            int(form_data.get("hiv_status")),
-            int(form_data.get("bacteriologically_confirmed")),
-            int(form_data.get("rif_resistance_detected")),
-            int(form_data.get("rif_resistance_not_detected")),
-            int(form_data.get("rr_tb")),
-            float(form_data.get("age")),
-            float(form_data.get("height")),
-            float(form_data.get("weight")),
-            int(form_data.get("days_in_treatment"))
+            float(form_data.get("respiratory_rate")),
+            float(form_data.get("c_reactive_proteins")),
+            int(form_data.get("age")),
+            float(form_data.get("tlc_count")),
         ]
 
         # Check feature length
         print("Parsed Features:", features)
 
-        if len(features) != 11:
+        if len(features) != 4:
             return "Error: Incorrect number of features provided", 400
 
         # Get prediction
         prediction = make_prediction(model, features)
 
         if prediction == 0:
-            res = "Cured"
-        elif prediction == 1:
-            res = "Died" 
-        elif prediction == 2:
-            res = "LOST_TO_FOLLOW_UP"
-        elif prediction == 3:
-            res = "TREATMENT_COMPLETE"
+            res = "Not Severe"
         else:
-            res = "Treatment Success"
+            res = "Severe Condition"
 
         return render_template("result.html", prediction=res)
 
     except Exception as e:
         print("Error:", str(e))
         return f"An error occurred: {str(e)}", 500
-
-if __name__ == "__main__":
-    app.run(debug=True)
